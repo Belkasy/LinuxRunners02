3edd3b5c6c690fe42d885b97fc647741813032f2
Abdelrahman Belkasy <90921667+Belkasy@users.noreply.github.com>
2022-09-18 21:05:26 +0200

    Add files via upload

--- 01lec/assin/myFemtoShell.c
+++ 01lec/assin/myFemtoShell.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <string.h>
 
+
+int main ()
+{
+	char str[100];
+	while(  ( strcmp(str,"exit") )  )
+	{
+		printf("Ans Gahz Ya basha > ");
+		gets(str);
+		printf("You said > %s\n",str);
+	}
+	printf("Good Bye ;)\n");
+	return 0;
+}
